88390b115afb98395dcd094bc61efda83cd453f0
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-12-27 15:46:54 +0530

    status updated

--- controllers/get_locker.js
+++ controllers/get_locker.js
@@ -4,9 +4,9 @@ const get_locker = async (req, res, next) => {
   const { id } = req.params;
   const locker = await model.findOne({ name: id }).exec();
   if (locker) {
-    res.json({ message: 0 });
+    res.json({ status: 0 });
   } else {
-    res.json({ error: "Locker not found" });
+    res.json({ status: 1 });
   }
 };
 
