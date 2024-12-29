afc3143a96bca7abc9febceba16d7b3e1567171c
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-12-29 16:48:01 +0530

    minor bug fixes

--- controllers/get_locker.js
+++ controllers/get_locker.js
@@ -1,12 +1,12 @@
 import model from "../schema/locker.js";
 
 const get_locker = async (req, res, next) => {
-  const { id } = req.params;
-  const locker = await model.findOne({ name: id }).exec();
+  const { name } = req.body;
+  const locker = await model.findOne({ name: name }).exec();
   if (locker) {
-    res.json({ status: 0 });
-  } else {
     res.json({ status: 1 });
+  } else {
+    res.json({ status: 0 });
   }
 };
 
