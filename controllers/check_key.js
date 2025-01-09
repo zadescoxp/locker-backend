0da49ed7ba14a26343952b103306d09a15bdafe8
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2025-01-10 02:05:40 +0530

    presigned data added to array

--- controllers/check_key.js
+++ controllers/check_key.js
@@ -1,13 +1,37 @@
 import model from "../schema/locker.js";
 import bcrypt from "bcrypt";
+import { s3 } from "../config/aws.js";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import dotenv from "dotenv";
+import { decryptObjectValues } from "../config/utils.js";
+
+dotenv.config();
 
 const check_key = async (req, res, next) => {
-  const { name, key } = req.body;
+  const { name, key } = decryptObjectValues(req.body);
   const locker = await model.findOne({ name: name }).exec();
   if (locker) {
     const match = await bcrypt.compare(key, locker.passkey);
     if (match) {
-      res.json({ status: 1, name: locker.name, data: locker.data });
+      let lockerData = [];
+      if (locker.data.length > 0) {
+        for (let i = 0; i < locker.data.length; i++) {
+          const getObjectParams = {
+            Bucket: process.env.BUCKET_NAME,
+            Key: locker.data[i].fileName,
+          };
+          const command = new GetObjectCommand(getObjectParams);
+          const signedUrl = await getSignedUrl(s3, command, {
+            expiresIn: 3600,
+          });
+          lockerData.push({
+            fileName: locker.data[i].fileName,
+            url: signedUrl,
+          });
+        }
+      }
+      res.json({ status: 1, name: locker.name, data: lockerData });
     } else {
       res.status(400).json({ status: 0, message: "Incorrect Passkey" });
     }
